0c3814fde312930c84218084342a8c024d4fc425
Joel Coulon <Joel.Coulon@warwick.ac.uk>
2025-12-07 11:42:34 +0000

    slightly better if logic

--- day7/pt1.py
+++ day7/pt1.py
@@ -9,13 +9,14 @@ num_splits = 0
 for i, line in enumerate(lines[1:], start=1):
     new_row = [False] * len(line)
     for j, c in enumerate(line):
-        if c == "^" and tachyon_beams[i - 1][j]:
-            num_splits += 1
-            if j > 0:
-                new_row[j - 1] = True
-            if j < len(line) - 1:
-                new_row[j + 1] = True
-        elif tachyon_beams[i - 1][j]:
-            new_row[j] = True
+        if tachyon_beams[i - 1][j]:
+            if c == "^":
+                num_splits += 1
+                if j > 0:
+                    new_row[j - 1] = True
+                if j < len(line) - 1:
+                    new_row[j + 1] = True
+            else:
+                new_row[j] = True
     tachyon_beams.append(new_row)
 print(num_splits)
--- day7/pt2.py
+++ day7/pt2.py
@@ -8,12 +8,13 @@ tachyon_beams = [[1 if c == "S" else 0 for c in lines[0]]]
 for i, line in enumerate(lines[1:], start=1):
     new_row = [0] * len(line)
     for j, c in enumerate(line):
-        if c == "^" and tachyon_beams[i - 1][j]:
-            if j > 0:
-                new_row[j - 1] += tachyon_beams[i - 1][j]
-            if j < len(line) - 1:
-                new_row[j + 1] += tachyon_beams[i - 1][j]
-        elif tachyon_beams[i - 1][j]:
-            new_row[j] += tachyon_beams[i - 1][j]
+        if tachyon_beams[i - 1][j]:
+            if c == "^":
+                if j > 0:
+                    new_row[j - 1] += tachyon_beams[i - 1][j]
+                if j < len(line) - 1:
+                    new_row[j + 1] += tachyon_beams[i - 1][j]
+            else:
+                new_row[j] += tachyon_beams[i - 1][j]
     tachyon_beams.append(new_row)
 print(sum(tachyon_beams[-1]))
