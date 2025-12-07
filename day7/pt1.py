334179d2e5ca9ff22d1c2604e472fd4547066101
Joel Coulon <Joel.Coulon@warwick.ac.uk>
2025-12-07 10:45:07 +0000

    day 7

---
+++ day7/pt1.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+with Path("input.txt").open() as f:
+    lines = f.read().splitlines()
+
+tachyon_beams = [[c == "S" for c in lines[0]]]
+
+num_splits = 0
+for i, line in enumerate(lines[1:], start=1):
+    new_row = [False] * len(line)
+    for j, c in enumerate(line):
+        if c == "^" and tachyon_beams[i - 1][j]:
+            num_splits += 1
+            if j > 0:
+                new_row[j - 1] = True
+            if j < len(line) - 1:
+                new_row[j + 1] = True
+        elif tachyon_beams[i - 1][j]:
+            new_row[j] = True
+    tachyon_beams.append(new_row)
+print(num_splits)
